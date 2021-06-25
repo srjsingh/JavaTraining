0c41e5937472bf8a667df8ab56d4104b184f0a61
Suraj <Suraj@DESKTOP-1EKAV2N>
2021-06-25 17:08:01 +0530

    First Commit - Java basic training

---
+++ JavaTraining/src/javaBasics/DataTypes_01.java
@@ -0,0 +1,29 @@
+package javaBasics;
+
+public class DataTypes_01 {
+
+	public static void main(String[] args) {
+		
+		// primitive data type int, char, double and boolean 
+		
+		// 1. integer
+		int i = 10;
+		
+		// 2. double is used for decimal
+		double d = 12.33; 
+		
+		// 3. char is for character
+		char c = 'A'; 
+		
+		// 4. boolean is either true or false
+		boolean b1 = true; 
+		boolean b2 = false;
+		
+		// String is a class and not data type
+		String s = "suraj";
+		
+		System.out.println(i + "," + d + "," + c + "," + b1 + "," + b2 + "," + s); 
+
+	}
+
+}
