243ad56c4b7ef459cd3692c169ab158ab32ccc6c
Suraj <Suraj@DESKTOP-1EKAV2N>
2021-06-26 15:50:46 +0530

    loop and increment/decrement concept

---
+++ JavaTraining/src/javaBasics/LoopsConcept_03.java
@@ -0,0 +1,42 @@
+package javaBasics;
+
+import java.util.Iterator;
+
+public class LoopsConcept_03 {
+
+	public static void main(String[] args) {
+		
+		// 1. while loop:
+		// disadvantage: It will give infinite loop if incremental or decremental condition is not given
+		
+		int i =1; // initialization
+		
+		while(i<=5) { // conditional part
+			
+			System.out.println(i);
+			
+			i = i+1; // incremental part
+			
+		}
+		
+		System.out.println("***********************");
+		
+	// 2. for loop
+		// j++ or j=j+1 is same
+		
+		for(int j = 1; j<=5; j++) { //Initialization, Conditional and Incremental/Decremental
+			
+			System.out.println(j);
+			
+		}
+		
+		System.out.println("********************************");
+		
+		for(int k=5; k>=1; k--) {
+			
+			System.out.println(k);
+			
+		}
+	}
+
+}
