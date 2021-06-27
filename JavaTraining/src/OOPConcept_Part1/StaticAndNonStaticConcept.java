7fe30bdc709a334f80be0551d888273ba5e988f0
Suraj <Suraj@DESKTOP-1EKAV2N>
2021-06-27 16:34:17 +0530

    static and non-static concept

---
+++ JavaTraining/src/OOPConcept_Part1/StaticAndNonStaticConcept.java
@@ -0,0 +1,47 @@
+package OOPConcept_Part1;
+
+public class StaticAndNonStaticConcept {
+	
+	//golbal vars: the scope of global vars will be available across all the functions with some conditions.
+
+	String name = "Tom"; // non static global var
+	static int age = 20; // static global var
+	
+	public static void main(String[] args) {
+		
+		// how to all static methods
+		//1. directly by name
+		sum();
+		
+		//2. calling by class name
+		StaticAndNonStaticConcept.sum();
+		
+		// how to call static vars
+		System.out.println(age); // by name
+		System.out.println(StaticAndNonStaticConcept.age); // by class name
+		
+		StaticAndNonStaticConcept obj = new StaticAndNonStaticConcept();
+		System.out.println(obj.name);
+		System.out.println(obj.age); // static var by object reference
+		obj.sendMail();
+		
+		//Can I access static methods by using object reference? yes
+		obj.sum();
+		
+		
+
+	}
+	
+	public void sendMail() { // non static method
+		
+		System.out.println("send mail method");
+		
+	}
+	
+	public static void sum() { // static method
+		
+		System.out.println("sum method");
+		
+	}
+
+}
