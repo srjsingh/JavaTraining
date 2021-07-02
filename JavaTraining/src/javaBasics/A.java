b979c4263c8ca7db709890c89e973f75ca164188
srjsingh <Srjsingh636@gmail.com>
2021-07-02 23:50:31 +0530

    formatting

--- JavaTraining/src/javaBasics/A.java
+++ JavaTraining/src/javaBasics/A.java
@@ -1,18 +1,20 @@
 package javaBasics;
 
 public class A {
-	
+
 	public A() {
 		System.out.println("Parent class const");
 	}
+
 	public A(int i) {
 		System.out.println("single param const");
-		System.out.println("Value of i: "+i);
+		System.out.println("Value of i: " + i);
 	}
+
 	public A(int i, int j) {
 		System.out.println("two param const");
-		System.out.println("Value of i: "+i);
-		System.out.println("Value of j: "+j);
+		System.out.println("Value of i: " + i);
+		System.out.println("Value of j: " + j);
 	}
 
 }
--- JavaTraining/src/javaBasics/B.java
+++ JavaTraining/src/javaBasics/B.java
@@ -1,26 +1,28 @@
 package javaBasics;
 
-public class B extends A{
+public class B extends A {
 
 	public B() {
-		super(); //super is used to call constructor of parent class
+		super(); // super is used to call constructor of parent class
 		// super should always be the first call in constructor
-		//there cannot be more than 1 super keyword in constructor
+		// there cannot be more than 1 super keyword in constructor
 		System.out.println("child class constructor");
 	}
+
 	public B(int i) {
 		super(i);
 	}
+
 	public B(int i, int j) {
-		super(i,j);
+		super(i, j);
 	}
-	
+
 	public static void main(String[] args) {
 
 		B obj = new B();
 		B obj1 = new B(10);
-		B obj2 = new B(10,20);
-		
+		B obj2 = new B(10, 20);
+
 	}
 
 }
