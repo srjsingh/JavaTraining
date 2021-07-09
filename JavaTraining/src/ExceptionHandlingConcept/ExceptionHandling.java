c01ff09fd66ee6f09aa1cf57cb7be3f3df58a49a
srjsingh <Srjsingh636@gmail.com>
2021-07-09 12:47:51 +0530

    throw keyword

--- JavaTraining/src/ExceptionHandlingConcept/ExceptionHandling.java
+++ JavaTraining/src/ExceptionHandlingConcept/ExceptionHandling.java
@@ -1,4 +1,4 @@
-package javaBasics;
+package ExceptionHandlingConcept;
 
 public class ExceptionHandling {
 
---
+++ JavaTraining/src/ExceptionHandlingConcept/ThrowKeyword.java
@@ -0,0 +1,10 @@
+package ExceptionHandlingConcept;
+
+public class ThrowKeyword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
